8eafe44f2a59f0e367ee75511db655142806f87f
zxs-ai <102594181+zxs-ai@users.noreply.github.com>
2025-03-08 23:59:10 +0800

    Update zzmusicplay.js

--- zzmusicplay.js
+++ zzmusicplay.js
@@ -10,7 +10,7 @@ if (body) {
       // 将歌手改为固定文本“点击播放—>”
       obj.data.song_singer = "z先生定制电台>>>";
       // 修改封面为指定链接
-      obj.data.cover = "https://gitee.com/applexyz/iosgj/raw/master/%E7%B4%A0%E6%9D%90%E6%96%87%E4%BB%B6/zz.jpg";
+      obj.data.cover = "https://gitee.com/applexyz/iosgj/raw/master/%E7%B4%A0%E6%9D%90%E6%96%87%E4%BB%B6/zxs-pic.jpeg";
     }
     $done({body: JSON.stringify(obj)});
   } catch (e) {
