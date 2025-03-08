90eea59a54311f9733991e556af2bb75cb60671e
zxs-ai <102594181+zxs-ai@users.noreply.github.com>
2025-03-08 23:37:16 +0800

    Create zdiy.js

---
+++ zdiy.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/zxs-ai/Xnet/refs/heads/main/zzmusicplay.js
+
+[mitm] 
+hostname = api.dragonlongzhu.cn
