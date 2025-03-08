516c7b4aebdc8cfc343d988d701692b3e39cc74a
zxs-ai <102594181+zxs-ai@users.noreply.github.com>
2025-03-09 06:44:35 +0800

    Update zdiy.js

--- zdiy.js
+++ zdiy.js
@@ -1,5 +1,5 @@
 [rewrite_local]
 ^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/zxs-ai/Xnet/refs/heads/main/zzmusicplay.js
 
-[mitm] 
+[mitm]
 hostname = api.dragonlongzhu.cn
