c366cc5591923595247f996ef1df27aace6f47a8
zxs-ai <102594181+zxs-ai@users.noreply.github.com>
2025-03-08 23:22:56 +0800

    Update zzmusicplay.js

--- zzmusicplay.js
+++ zzmusicplay.js
@@ -1,23 +1,14 @@
-// mikoto - 修改点歌卡片的封面
-//使用方法：圈x 添加主机名  api.dragonlongzhu.cn
-//修改本配置的文件名为mkqqyy.js
-//把这个配置放到文件app-quantumultx-Scripts
-//修改本配置显示文本和封面链接
-//然后点歌
-
-
 let body = $response.body;
 if (body) {
   try {
     let obj = JSON.parse(body);
     if (obj && obj.data) {
-   
       let originalName = obj.data.song_name || "";
       let originalSinger = obj.data.song_singer || "";
-    
+      
       obj.data.song_name = originalName + "-" + originalSinger;
       // 将歌手改为固定文本“点击播放—>”
-      obj.data.song_singer = "zz在线播放 >>>";
+      obj.data.song_singer = "z先生定制电台>>>";
       // 修改封面为指定链接
       obj.data.cover = "https://gitee.com/applexyz/iosgj/raw/master/%E7%B4%A0%E6%9D%90%E6%96%87%E4%BB%B6/zz.jpg";
     }
