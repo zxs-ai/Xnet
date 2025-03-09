291faf28a9760788f46981c6f01a63e99de604d1
zxs-ai <102594181+zxs-ai@users.noreply.github.com>
2025-03-09 09:54:23 +0800

    Update zzmusicplay.js

--- zzmusicplay.js
+++ zzmusicplay.js
@@ -1,21 +1,22 @@
-/*
- * zzmusicplay.js
- * 此脚本用于重写 api.dragonlongzhu.cn 接口返回的 JSON 数据，
- * 将歌曲名称与歌手名称拼接，将歌手替换为固定文本，
- * 并将封面修改为指定链接。
- *       obj.data.cover = "https://gitee.com/applexyz/iosgj/raw/master/diymusic/zz.jpg";
- */
+//mikoto - 修改点歌卡片的封面
+//使用方法：圈x 添加主机名  api.dragonlongzhu.cn
+//修改本配置的文件名为mkqqyy.js
+//把这个配置放到文件app-quantumultx-Scripts
+//修改本配置显示文本和封面链接
+//然后点歌
+
+
 let body = $response.body;
 if (body) {
   try {
     let obj = JSON.parse(body);
     if (obj && obj.data) {
-      
+   
       let originalName = obj.data.song_name || "";
       let originalSinger = obj.data.song_singer || "";
-      // 拼接歌曲名与歌手名
+    
       obj.data.song_name = originalName + "-" + originalSinger;
-      // 固定歌手名称
+      // 将歌手改为固定文本“点击播放—>”
       obj.data.song_singer = "z先生定制电台>>>";
       // 修改封面为指定链接
       obj.data.cover = "https://s3.bmp.ovh/imgs/2025/03/09/1fc6b8a5c62d8236.png";
