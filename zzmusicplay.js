4cd4af8af71ec5f7730259d70c11f4bfb74172c1
zxs-ai <102594181+zxs-ai@users.noreply.github.com>
2025-03-09 09:30:37 +0800

    Update zzmusicplay.js

--- zzmusicplay.js
+++ zzmusicplay.js
@@ -3,6 +3,7 @@
  * 此脚本用于重写 api.dragonlongzhu.cn 接口返回的 JSON 数据，
  * 将歌曲名称与歌手名称拼接，将歌手替换为固定文本，
  * 并将封面修改为指定链接。
+ *       obj.data.cover = "https://gitee.com/applexyz/iosgj/raw/master/diymusic/zz.jpg";
  */
 let body = $response.body;
 if (body) {
@@ -17,7 +18,7 @@ if (body) {
       // 固定歌手名称
       obj.data.song_singer = "z先生定制电台>>>";
       // 修改封面为指定链接
-      obj.data.cover = "https://gitee.com/applexyz/iosgj/raw/master/diymusic/zz.jpg";
+      obj.data.cover = "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__cff92031-5429-42d4-a3fe-5ddcd20b13b6_1741445606820.jpg";
     }
     $done({body: JSON.stringify(obj)});
   } catch (e) {
